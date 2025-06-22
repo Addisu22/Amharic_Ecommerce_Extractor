14766db6d287bfa477bce2b653b00088fff54cad
Addisu22 <liveaddisu@gmail.com>
2025-06-23 00:01:29 +0300

    Data Ingestion and  Data Preprocessing

--- scripts/preprocess.py
+++
@@ -1,8 +0,0 @@
-import re
-
-def normalize_amharic(text):
-    text = re.sub(r'[^\w\s፣።፤፥]', '', text)  # Remove strange symbols
-    return text.strip()
-
-def tokenize_amharic(text):
-    return text.split()
